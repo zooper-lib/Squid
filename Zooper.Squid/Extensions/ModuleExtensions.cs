da9ef023dbc439666f9feaaa1377befb1fb3375d
Daniel <daniel@zooper.dev>
2025-04-18 20:16:03 +0200

    Initial implementation

---
+++ Zooper.Squid/Extensions/ModuleExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Zooper.Squid.Modules;
+
+namespace Zooper.Squid.Extensions;
+
+public static class ModuleExtensions
+{
+	public static WebApplicationBuilder AddModules(
+		this WebApplicationBuilder builder,
+		params Type[] moduleTypes)
+	{
+		foreach (var moduleType in moduleTypes)
+		{
+			if (Activator.CreateInstance(moduleType) is not AppModule module) continue;
+
+			module.ConfigureServices(builder);
+			builder.Services.AddSingleton(module);
+		}
+
+		return builder;
+	}
+
+	public static WebApplication UseModules(this WebApplication app)
+	{
+		var modules = app.Services.GetServices<AppModule>();
+
+		foreach (var module in modules)
+		{
+			module.ConfigureMiddleware(app);
+		}
+
+		return app;
+	}
+}
---
+++ Zooper.Squid/Modules/AppModule.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Zooper.Squid.Modules;
+
+public abstract class AppModule
+{
+	public virtual void ConfigureServices(WebApplicationBuilder builder) { }
+	public virtual void ConfigureMiddleware(WebApplication app) { }
+}
