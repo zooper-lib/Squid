f6f5300a68375a6093a935b1b6cfc7947387e72c
Daniel <daniel@zooper.dev>
2025-04-18 20:27:21 +0200

    Added workflows

--- Zooper.Squid/Extensions/ModuleExtensions.cs
+++ Zooper.Squid/Extensions/ModuleExtensions.cs
@@ -4,8 +4,23 @@ using Zooper.Squid.Modules;
 
 namespace Zooper.Squid.Extensions;
 
+/// <summary>
+/// Provides extension methods for working with <see cref="AppModule"/> instances in an ASP.NET Core application.
+/// </summary>
 public static class ModuleExtensions
 {
+	/// <summary>
+	/// Registers and configures the specified modules in the application's service collection.
+	/// </summary>
+	/// <param name="builder">The <see cref="WebApplicationBuilder"/> to add the modules to.</param>
+	/// <param name="moduleTypes">The types of the modules to add to the application.</param>
+	/// <returns>The <see cref="WebApplicationBuilder"/> for chaining.</returns>
+	/// <example>
+	/// <code>
+	/// var builder = WebApplication.CreateBuilder(args);
+	/// builder.AddModules(typeof(MyModule), typeof(AnotherModule));
+	/// </code>
+	/// </example>
 	public static WebApplicationBuilder AddModules(
 		this WebApplicationBuilder builder,
 		params Type[] moduleTypes)
@@ -21,6 +36,17 @@ public static class ModuleExtensions
 		return builder;
 	}
 
+	/// <summary>
+	/// Configures the middleware pipeline for all registered modules.
+	/// </summary>
+	/// <param name="app">The <see cref="WebApplication"/> to configure.</param>
+	/// <returns>The <see cref="WebApplication"/> for chaining.</returns>
+	/// <example>
+	/// <code>
+	/// var app = builder.Build();
+	/// app.UseModules();
+	/// </code>
+	/// </example>
 	public static WebApplication UseModules(this WebApplication app)
 	{
 		var modules = app.Services.GetServices<AppModule>();
--- Zooper.Squid/Modules/AppModule.cs
+++ Zooper.Squid/Modules/AppModule.cs
@@ -3,8 +3,32 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Zooper.Squid.Modules;
 
+/// <summary>
+/// Base class for creating modular components in an ASP.NET Core application.
+/// </summary>
+/// <remarks>
+/// This class provides a foundation for creating modular applications by allowing each module to configure
+/// its own services and middleware. Modules can be added to the application using the <see cref="ModuleExtensions"/> class.
+/// </remarks>
 public abstract class AppModule
 {
+	/// <summary>
+	/// Configures services required by this module.
+	/// </summary>
+	/// <param name="builder">The <see cref="WebApplicationBuilder"/> used to configure the application.</param>
+	/// <remarks>
+	/// Override this method to add services to the dependency injection container.
+	/// This method is called during application startup before the middleware pipeline is configured.
+	/// </remarks>
 	public virtual void ConfigureServices(WebApplicationBuilder builder) { }
+
+	/// <summary>
+	/// Configures the middleware pipeline for this module.
+	/// </summary>
+	/// <param name="app">The <see cref="WebApplication"/> used to configure the middleware pipeline.</param>
+	/// <remarks>
+	/// Override this method to add middleware to the application's request pipeline.
+	/// This method is called during application startup after services have been configured.
+	/// </remarks>
 	public virtual void ConfigureMiddleware(WebApplication app) { }
 }
